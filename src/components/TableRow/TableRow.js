5754b3834f0e51f171262e300e00bfb43fc8e8f6
Moses Burkhart <moses.burkhart1@gmail.com>
2022-04-12 17:59:21 -0400

    slight tweak to prices page

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function TableRow({symbol, name, change, price, percentChange}){
     return (
         <tr>
-            <td><Link to={`/stocks/${symbol}`} state={{company: {name: name, symbol: symbol}}}>{name} ({symbol})</Link></td>
+            <td><Link to={`/stocks/${symbol}`} state={{company: {name: name, symbol: symbol, change: change, price: price, percentChange: percentChange}}}>{name} ({symbol})</Link></td>
             <td>{price}</td>
             <td className={percentChange > 0 ? 'pos' : 'neg'}>{change} ({Math.round(percentChange*100)/100}%)</td>
         </tr>
--- src/pages/Prices/Prices.js
+++ src/pages/Prices/Prices.js
@@ -2,20 +2,16 @@ import { useParams, useLocation } from "react-router-dom";
 import {useState, useEffect} from 'react';
 
 export default function Prices({data}) {
-    const [company, setCompany] = useState([]);
     const params = useParams();
     const symbol = params.symbol;
-    // const location = useLocation();
-    // const {company} = location.state;
+    const location = useLocation();
+    const {company} = location.state;
 
-    const getCompany = async () => {
-        setCompany(data.filter(stock => stock.symbol === symbol));
-    }
 
     const getInfo = () => {
-        const rows = Object.entries(company[0]).map(([key,value], idx) => {
+        const rows = Object.entries(company).map(([key,value], idx) => {
             return (
-                <h2 className={key !== 'changesPercentage' && key !== 'change' ? '' : value < 0 ? 'neg' : 'pos'} key={idx}>{key === 'changesPercentage' ? "Percentage Change" : key}: {key === 'changesPercentage' ? `${Math.round(value*100)/100}%` : value}</h2>
+                <h2 className={key !== 'percentChange' && key !== 'change' ? '' : value < 0 ? 'neg' : 'pos'} key={idx}>{key === 'percentChange' ? "Percentage Change" : key}: {key === 'changesPercentage' ? `${Math.round(value*100)/100}%` : value}</h2>
             )
         })
         return rows
@@ -33,11 +29,7 @@ export default function Prices({data}) {
         return <h1>Loading ...</h1>
     }
 
-    useEffect(() => {
-        getCompany();
-    },[])
-
-    return data[0] && company[0] ? loaded() : loading();
+    return company ? loaded() : loading();
 
 
 }
